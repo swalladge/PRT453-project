241ce28cfa76490e6fbf17d2132eeb04499a542f
Samuel Walladge <samuel@swalladge.id.au>
2018-04-17 09:15:26 +0930

    don't allow year in future

--- src/Car/YearValidator.java
+++ src/Car/YearValidator.java
@@ -1,11 +1,20 @@
 package Car;
 
+import java.time.Year;
+
 public class YearValidator implements CarFieldValidator {
     @Override
     public void validate(CarFields carFields) throws FieldValidationException {
         Integer year = carFields.getYear();
+
+        // ensure sane values - should be ok for a few thousand years
         if (year < 1000 || year > 9999) {
             throw new FieldValidationException("Year not valid (too large or too small)");
+        }
+
+        // can't add something from the future!
+        if (year > (Year.now().getValue())) {
+            throw new FieldValidationException("Year cannot be in the future.");
         }
     }
 }
